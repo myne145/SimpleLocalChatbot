5b0d28d352b2e4b401948c396968c25e6e138986
Oskar Przybylski <155388816+oskar-przybylski@users.noreply.github.com>
2025-02-25 12:36:55 +0100

    Dead code cleanup

--- src/com/tenx/simplechatbot/json/ResponseJsonParser.java
+++ src/com/tenx/simplechatbot/json/ResponseJsonParser.java
@@ -1,47 +1,9 @@
 package com.tenx.simplechatbot.json;
 
-import com.tenx.simplechatbot.utils.Choice;
-import com.tenx.simplechatbot.utils.ModelStats;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 public class ResponseJsonParser {
-    private final ArrayList<Choice> choices = new ArrayList<>();
-    private ModelStats modelStats;
-
-    private long creationDateSeconds;
-
-    private static Choice parseChoice(JSONObject choiceJSON) {
-        JSONObject message = choiceJSON.getJSONObject("message");
-        Choice choice = new Choice(
-                choiceJSON.getInt("index"),
-                message.getString("role"),
-                message.getString("content")
-        );
-        return choice;
-    }
-
-    public ResponseJsonParser(JSONObject response) {
-        //filling the choices array
-        JSONArray array = response.getJSONArray("choices");
-        for(int i = 0; i < array.length(); i++) {
-            choices.add(parseChoice(array.getJSONObject(i)));
-        }
-
-        //model stats
-        JSONObject stats = response.getJSONObject("stats");
-        modelStats = new ModelStats(
-                stats.getDouble("tokens_per_second"),
-                stats.getDouble("time_to_first_token"),
-                stats.getDouble("generation_time"),
-                stats.getString("stop_reason")
-        );
-
-        //creation date
-        creationDateSeconds = response.getLong("created");
-    }
 
     public static String parsePrompt(String prompt,String system,String model){
         // tworzy JSON w postaci stringa z danymi argumentami. Gotowe pole data do wyslania do com.tenx.simplechatbot.lmapi.API
